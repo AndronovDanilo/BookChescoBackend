7b4c6257b738f7ea33d6eed438ab4f1e14ef0e4c
AndronovDanilo <azumalive@gmail.com>
2025-11-12 15:12:53 +0200

    a

--- BookChescoInfrastructure/Configuration/AppDbContext.cs
+++ BookChescoInfrastructure/Configuration/AppDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext : DbContext
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<Room> Rooms { get; set; }
     public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Photo> Photos { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -16,6 +17,32 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Hotel>()
+            .HasMany(h => h.Rooms)
+            .WithOne(r => r.Hotel)
+            .HasForeignKey(r => r.HotelId)
+            .OnDelete(DeleteBehavior.Restrict);
         
+        modelBuilder.Entity<Room>()
+            .HasMany(r => r.Bookings)
+            .WithOne(b => b.Room)
+            .HasForeignKey(b => b.RoomId)
+            .OnDelete(DeleteBehavior.SetNull);
+        
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Bookings)
+            .WithOne(b => b.User)
+            .HasForeignKey(b => b.UserId)
+            .OnDelete(DeleteBehavior.SetNull);
+        
+        modelBuilder.Entity<Hotel>()
+            .HasMany(h => h.Photos)
+            .WithMany(p => p.Hotels)
+            .UsingEntity(j => j.ToTable("HotelPhotos"));
+        
+        modelBuilder.Entity<Room>()
+            .HasMany(r => r.Photos)
+            .WithMany(p => p.Rooms)
+            .UsingEntity(j => j.ToTable("RoomPhotos"));
     }
 }
